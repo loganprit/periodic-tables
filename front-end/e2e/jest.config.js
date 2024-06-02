9284421ba0039621be16e66f1ca21292094dc829
Logan Pritchett <logan.pritchett01@gmail.com>
2024-06-02 16:50:01 -0500

    chore: Update dependencies and Jest config

--- front-end/e2e/jest.config.js
+++ front-end/e2e/jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   preset: "jest-puppeteer",
+  testEnvironment: "jest-environment-puppeteer",
+  testEnvironmentOptions: {
+    resources: "usable",
+  },
   testTimeout: 8000,
 };
