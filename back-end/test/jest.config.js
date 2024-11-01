116da9586539b63ba63de6a53451bbe33ce2571e
Logan Pritchett <logan.pritchett01@gmail.com>
2024-10-31 19:35:13 -0500

    test: refactor jest for TS

--- back-end/test/jest.config.js
+++ back-end/test/jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  roots: ["<rootDir>/../src"],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  coverageDirectory: "../coverage",
   testTimeout: 20000,
 };
