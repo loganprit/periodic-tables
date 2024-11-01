e761e65befb9cfe0b9fca70fb40929326f4a9d03
Logan Pritchett <logan.pritchett01@gmail.com>
2024-10-31 22:27:28 -0500

    fix: update app.ts to use updated routers

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -7,8 +7,8 @@ import { CustomRequest, CustomResponse } from "./types/application";
 
 import errorHandler from "./errors/errorHandler";
 import notFound from "./errors/notFound";
-import { reservationsRouter } from "./reservations/reservations.router";
-import { tablesRouter } from "./tables/tables.router";
+import reservationsRouter from "./reservations/reservations.router";
+import tablesRouter from "./tables/tables.router";
 
 // Configure environment variables
 dotenv.config({ path: path.join(__dirname, "..", ".env") });
